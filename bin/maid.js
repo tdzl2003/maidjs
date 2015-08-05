937db0cdf35e3e6762901309e1134ef662c21d47
DengYun <tdzl2003>
2015-08-05 13:48:34 +0800

    use babel/register instead of babel-node

--- bin/maid.js
+++ bin/maid.js
@@ -1,57 +1,11 @@
-#!/usr/bin/env babel-node
-"use strict";
+#!/usr/bin/env node
 
-if (require.main !== module){
-    throw new Error("This file should be run from cli.");
-}
+require("babel/register")({
+    "ignore": false,
+    "whitelist": [
+        "es6.arrowFunctions",
+        "es6.destructuring"
+    ]
+})
 
-var cliArgv = require("cli-arguments");
-var path = require("path");
-var os = require("os");
-var co = require("co");
-var argvJson = require("./maid-argv.json");
-var options, command, args;
-try{
-    options = cliArgv.parse(argvJson);
-    command = options.command;
-    args = options.args;
-    options = options.options;
-} catch (e){
-    console.error(e.stack);
-    command = "help";
-    args = [];
-    options = {};
-}
-
-
-var Client = require("../lib/client");
-
-if (command == 'help') {
-    cliArgv.displayHelp(argvJson, args[0]);
-} else if (command == 'start') {
-    co(new Client(options).startServer())
-        .catch( e =>{
-            console.log(e.stack);
-        });
-} else if (command == 'status'){
-    co(new Client(options).getStatus())
-        .then(status => {
-            if (status.running){
-                if (status.notResponding){
-                    console.log('status: running[not-responding]');
-                } else {
-                    console.log('status: running')
-                }
-            } else {
-                console.log('status: stopped');
-            }
-        }).catch(function(e){
-            console.error(e.stack);
-        });
-} else if (command == 'daemon'){
-    let Server = require("../lib/server");
-    co(new Server(options).start())
-        .catch(e => {
-            console.error(e.stack);
-        });;
-}
+require("../lib/cli");
